73eec26be6b19ca7239087a66198b7ef3638d53a
NoffarLevi <noffarlevi@gmail.com>
2019-12-14 15:59:45 +0200

    second

--- arrays.c
+++ arrays.c
@@ -9,5 +9,19 @@ int *p= (arr+i);
 *p= *(p-1);
 i--;
 }
+}
 
+void insertion_sort(int* arr , int len){
+
+for (int i=1; i<len; i++){
+
+
+for(int j=i; j>0; j--){
+if(*(arr+j)< *(arr+(j-1))){
+int temp=*(arr+j);
+shift_element((arr+(j-1)),1);
+*(arr+(j-1))=temp;
+}
+}
+}
 }
--- main.c
+++ main.c
@@ -2,25 +2,48 @@
 #include <math.h> 
 #include "arrays.c"
 
-#define	CHECKSIZE 4
-//#define  CHECK [] ={2,5,8,4}
-// #define int* pointer = CHECK;
-#define  SHIFTBY 2
-//gcc -o output 'main.c'
+#define	ARRAYSIZE 4
+int ARRAY [] ={1,3,6,4};
+int* pointer1 = ARRAY;
+#define  SHIFTBY 2 
+
+#define ARRSIZE 50
+int ARR[ARRSIZE]={0};
+int* pointer2 = ARR;
+
 int main(){
 
-int CHECK[] = {1,5,8,4};
-int shift=SHIFTBY;
+int insertedNum=0; 
+int s=0;
 
-shift_element(CHECK+1, shift);
-
-
-for (int loop=0; loop<CHECKSIZE; loop++){
-printf("%d ", *(CHECK+loop));
+/* Q1 part a */
+shift_element(pointer1, SHIFTBY);
+for (int loop=0; loop<ARRAYSIZE; loop++){ /* printing ARRAY */
+printf("%d \n", *(ARRAY+loop));
 }
 
+for (int lop=0; lop<ARRSIZE; lop++){   /* adding numbers from user into ARR */
+printf("Please Enter a Whole Number for Index %d: ", lop);
+s=scanf("%d ", &insertedNum); 
+/*while(s!=1){
+printf("Please Enter a Number :"); what if user enters something invalid
+s=scanf("%d ", &insertedNum); 
+} */
+*(ARR+lop)=insertedNum;
+}
+printf("ARR is : ");
+for (int lp=0; lp<ARRSIZE; lp++){  /* printing  ARR */
+printf("%d,", *(ARR+lp));
+}
+printf("\n");
 
-
+/* Q1 part b */
+insertion_sort(pointer2 , ARRSIZE); /* sorts ARR */
+printf("Sorted ARR is : ");
+for (int lp=0; lp<ARRSIZE; lp++){  /* printing sorted ARR */
+printf("%d,", *(ARR+lp));
+}
+printf("\n");
 
 return 0;
 }
